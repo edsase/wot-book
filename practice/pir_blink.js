c9272c2c2ac23e47b91cf99eb4738fd77277d690
Edmond Sesay <edmondssesay@yahoo.com>
2019-01-19 14:50:00 +0100

    pir-blink func added

---
+++ practice/pir_blink.js
@@ -0,0 +1,31 @@
+var Gpio = require('onoff').Gpio,
+  led = new Gpio(10, 'out');
+
+
+//init input pin 24 to both (means both rising and falling edge)
+var sensor = new Gpio(24, 'in', 'both');
+
+// listen for state change in input 24. 
+// if change, run callback func
+sensor.watch(function(err,value){
+    if (err) exit(err);
+    console.log(value ? 'There is someone' : 'not anymore!');
+    turnOnLED(value);
+})
+
+// function to activate LED if someone is detected
+var turnOnLED = function(value){
+    led.write(value, function(){
+        console.log('Changed LED state to ' + value);
+    })
+}
+
+// function to do clean exit
+function exit(err){
+    if (err) console.log('An error occured ' + err);
+    sensor.unexport();
+    console.log('Bye bye!')
+    process.exit();
+}
+
+process.on('SIGINT', exit)
