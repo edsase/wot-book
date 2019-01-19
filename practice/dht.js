c5df62764756e6e910f57499eb5948a543cf0a5d
Edmond Sesay <edmondssesay@yahoo.com>
2019-01-19 18:29:36 +0100

    code for temp and humidity added

---
+++ practice/dht.js
@@ -0,0 +1,13 @@
+var sensorLib = require('node-dht-sensor');
+
+sensorLib.initialixe(22,5);
+var interval = setInterval(function(){
+    read();
+}, 2000);
+
+function read(){
+    var readout = sensorLib.read();
+    console.log('Temperature: ' + readout.temperature.toFixed(2) + 'C, ' + 
+    'Humidity: ' + readout.humidity.toFixed(2) + '%');
+};
+
